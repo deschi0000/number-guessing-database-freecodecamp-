5c810273a5b4a30fd0ecbb38f3fb79483ced8509
Dave von Deschwanden <deschi0000@outlook.com>
2025-03-28 05:32:54 +0000

    feat: add guess func

--- number_guess.sh
+++ number_guess.sh
@@ -41,3 +41,29 @@ else
   echo -e "Welcome back, $USERNAME! You have played $NUM_GAMES_PLAYED games, and your best game took $FEWEST_GUESS_GAME guesses."
 fi
 
+
+START_GAME () {
+  echo -e "Guess the secret number between 1 and 1000:"
+  # Choose a random num between 1 - 1000
+  RANDOM_NUM=$((1 + $RANDOM % 1000))
+  echo $RANDOM_NUM
+
+  # Get user input and make sure it is an int
+  read USER_GUESS
+  if [[ ! $USER_GUESS =~ ^[0-9]+$ ]]
+    then 
+      while [[ ! $USER_GUESS =~ ^[0-9]+$ ]]
+        do
+          echo That is not an integer, guess again:
+          read USER_GUESS
+        done
+  fi
+  echo "your guess: $USER_GUESS"
+
+  # while [[ $USER_GUESS -ne RANDOM_NUM ]]
+
+}
+
+START_GAME
+
+
