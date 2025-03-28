33f90c19fd6734713140e5aae2dd0b20daeb2578
Dave von Deschwanden <deschi0000@outlook.com>
2025-03-28 05:15:59 +0000

    fix:readability in welcome

--- number_guess.sh
+++ number_guess.sh
@@ -19,19 +19,25 @@ if [[ -z $USERNAME_RESULT ]]
     INSERT_USER_RESULT=$($PSQL "INSERT INTO users (username)
                                   VALUES ('$USERNAME')")
     echo -e "Welcome, $USERNAME! It looks like this is your first time here."
+
+else
+  # Get number of games played
+  NUM_GAMES_PLAYED=$($PSQL "SELECT COUNT(games_id) FROM games 
+                              JOIN users USING (user_id)
+                              WHERE username = '$USERNAME'" )
+
+  # Get game with the fewest guess
+  FEWEST_GUESS_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games
+                              JOIN users USING (user_id)
+                              WHERE username = '$USERNAME'")
+  
+  # For neater printing in the echo
+  if [[ -z $FEWEST_GUESS_GAME ]]
+    then
+    FEWEST_GUESS_GAME=0
+  fi
+
+  # Else they exist and print back the stats
+  echo -e "Welcome back, $USERNAME! You have played $NUM_GAMES_PLAYED games, and your best game took $FEWEST_GUESS_GAME guesses."
 fi
-
-# Get number of games played
-NUM_GAMES_PLAYED=$($PSQL "SELECT COUNT(games_id) FROM games 
-                            JOIN users USING (user_id)
-                            WHERE username = '$USERNAME'" )
-
-# Get game with the fewest guess
-FEWEST_GUESS_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games
-                            JOIN users USING (user_id)
-                            WHERE username = '$USERNAME'")
-
-# Else they exist and print back the stats
-echo -e "Welcome back, $USERNAME! You have played $NUM_GAMES_PLAYED games, and your best game took $FEWEST_GUESS_GAME guesses."
-
 
